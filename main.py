5429328abc65ba1c118bc3f4cbc5bb4c83ee25d0
skalakm <skalakm@dickinson.edu>
2021-01-20 15:19:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from graphics import *
+
+for i in range(4):
+    print("Evan climbs rocks.")
+    print("Lev winds clocks.")
+    print("any diff?")
+
+win = GraphWin("Test", 100, 100)
+pCirc = Circle(Point(50, 50), 20)
+pCirc.setFill("pink")
+pCirc.draw(win)
