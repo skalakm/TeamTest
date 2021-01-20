a5de4e7bf78be1b29113d94b6ef73b8146fac32b
skalakm <skalakm@dickinson.edu>
2021-01-20 15:20:13 +0000

    did garbage

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from graphics import *
 for i in range(4):
     print("Evan climbs rocks.")
     print("Lev winds clocks.")
-    print("any diff?")
+    print("any diff?") aoeuoeu
 
 win = GraphWin("Test", 100, 100)
 pCirc = Circle(Point(50, 50), 20)
