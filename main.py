fc5248367cee301ab02cf47b78f8edf94adcb6ba
skalakm <skalakm@dickinson.edu>
2021-01-20 15:54:12 +0000

    aeoioeui

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from graphics import *
 
+
 for i in range(4):
     print("Evan climbs rocks.")
     print("Lev winds clocks.")
-    print("any diff?") aoeuoeu
+    print("any diff?")
 
 win = GraphWin("Test", 100, 100)
 pCirc = Circle(Point(50, 50), 20)
 pCirc.setFill("pink")
 pCirc.draw(win)
 print("aoeuaoeubalaoeub")
+
+print(input())
